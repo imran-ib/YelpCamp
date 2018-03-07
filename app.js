a043962588ad5bdd4a00157c0d70af24c5deb7ba
Imran Irshad <imran-ib@outlook.com>
2018-03-07 18:01:50 +0300

    working on Routes

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+var express         = require('express');
+var path            = require('path');
+var favicon         = require('serve-favicon');
+var logger          = require('morgan');
+var cookieParser    = require('cookie-parser');
+var bodyParser       = require('body-parser');
 
 var app = express();
 
@@ -22,25 +19,46 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// ========================================
+//    Routes
+// ========================================
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/' , function (req , res ) {
+    res.send("this will be our landing page")
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// ========================================
+//    Routes
+// ========================================
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// ========================================
+//    Routes
+// ========================================
+
+// ========================================
+//    Routes
+// ========================================
+
+// ========================================
+//    Routes
+// ========================================
+
+// ========================================
+//    Routes
+// ========================================
+
+// ========================================
+//    Routes
+// ========================================
+
+// ========================================
+//    Routes
+// ========================================
+
+
+// SERVER
+
+app.listen(3000 , function(req , res){
+  console.log("Sever Is Ruing on Port 3000")
 });
-
 module.exports = app;
